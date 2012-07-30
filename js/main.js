cd110b8b605e0d015734fa04fdc822d04b65b607
Adam Watson <awats.info@gmail.com>
2012-07-29 20:17:54 -0700

    Outputting view to the FE

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
-require(['utils/zepto','models/artist.model','views/artist.view'],function( $, ArtistM ){
- 	
-  var bio = new ArtistM.Bio();
-  
-      bio.on("error", function(model, error){ //checks for id at set
-  	    console.log("An error was found");
-  	    console.log(error);
-  	  });
-
-  
-  	  
-  	  bio.set({name: "radiohead"});
-      
-  	  	  
- 	  bio.fetch({error: function(model,error){ //checks for error on xhr	
-  	  	//console.log(error);
-  	   },
-  	   success: function(model,rObj){
-  	    //console.log(rObj.response.biographies);	
-  	   }
-  	  
-  	  });
+require(['utils/zepto','models/artist.model','views/artist.view'],function( $, ArtistM, ArtistV ){
+ 		
+	var bioView = new ArtistV.SingleBio({name:"radiohead"});
+	
 	  
 });
--- js/views/artist.view.js
+++ js/views/artist.view.js
@@ -1,24 +1,56 @@
-define(['config','utils/lodash','utils/backbone'],function(){
+define(['config', 'models/artist.model','utils/lodash','utils/backbone'],function( config,ArtistM ){
  
  var ArtistV = ArtistV || {};
 
 
- ArtistV.Bio = Backbone.View.extend({
- 
-  tagName: 'li',
-  className: 'document-row biography',
-  	
-  initaialize: function(){
+ ArtistV.SingleBio = Backbone.View.extend({
   
+  el: $('#bio-list'),
+  model: ArtistM.Bio,
+ // tagName: 'ul',
+ // id: 'bio-list',
+  	
+  initialize: function(){
+    
+    var _this = this;
+  	var artist = new this.model();
+	
+	artist.on("error", function(model, error){ //checks for id at set
+  	  console.log("An error was found");
+  	  console.log(error);
+  	});
+
+	
+	artist.set({name:this.options.name});
+	
+	
+	artist.fetch({error: function(model,error){ //checks for error on xhr	
+  	  	//console.log(error);
+  	   },
+  	   success: function(model,rObj){
+  	    
+  	    _this.biography = rObj.response.biographies[0];
+  	    _this.render();
+
+  	   }
+   });
   },
   events: {
   
   },
   render: function(){
-  
+  console.log(this.biography);
+   var biography = this.biography;
+   var li = '<li>'+ this.biography.text +' From: <a href="'+ biography.url +'" target="_blank">'+ biography.site +'</a></li>';
+   
+   
+   this.$el.html(li);
+   
   }
   
  });
 
+
+return ArtistV;
 
 });
