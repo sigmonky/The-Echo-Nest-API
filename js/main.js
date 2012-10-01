50286d80eeca32221f3f91cdb3ab2fa0e6283349
Adam Watson <awats.info@gmail.com>
2012-10-01 14:59:18 -0700

    Update js/main.js
    Right now this is a place to test api responses using crude views.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//testing api calls with this for now as they are created as collections and models
 require(['utils/zepto','models/artist.model','views/artist.view'],function( $, ArtistM, ArtistV ){
  		
 	var bioView = new ArtistV.SingleBio({name:"radiohead"});
